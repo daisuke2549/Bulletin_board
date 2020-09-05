f0c427958a7869a198855b73d8541119047b912e
daisuke sasaki <gerund25495641@gmail.com>
2020-09-05 09:49:54 +0900

    no message

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,16 @@
 class PostsController < ApplicationController
     
     def index
+       @posts = Post.all
     end
   
+    def show 
+        @post = Post.find(params[:id])
+    end 
+    
 
+    private
+    def post_params
+      params.require(:content).permit(:name)
+    end
 end
