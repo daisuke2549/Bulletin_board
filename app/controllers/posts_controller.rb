eede7276e1f89c06f97732570d6324544559cbee
daisuke sasaki <gerund25495641@gmail.com>
2020-09-08 06:08:46 +0900

    画像投稿機能を実装し、動作確認済み

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,57 +1,5 @@
 class PostsController < ApplicationController
     
-    def index
-       @posts = Post.all
-    end
-
-    def show 
-      @post = Post.find(params[:id])
-    end
-    
-    def edit 
-      @post= Post.find(params[:id])
-    end
-
-    def new
-        @post = Post.new
-    end
-
-    def create
-        @post = Post.new(post_params)
-        if @post.save!
-            redirect_to posts_path(@post), notice: '保存できました'
-          else
-            flash.now[:error] = '保存に失敗しました'
-            render :new
-        end
-    end
-
-    def update
-      @post = Post.find(params[:id])
-      @post.update params.require(:post).permit(:name, :content, images: [])
-      if @post.update(post_params)
-        redirect_to post_path(@post), notice: '更新できました'
-      else
-        flash.now[:error] = '更新できませんでした'
-        render :edit
-      end
-    end
-    
-    def destroy
-        @post = Post.find(params[:id])
-        @post.destroy!
-        redirect_to posts_path(@post), notice: '削除に成功しました'
-    end
-
-
-    private
-    def post_params
-        params.require(:post).permit(:name, :content, :images)
-    end
-    def set_post
-        @post = Post.find(params[:id])
-    end
-=======
   def index
      @posts = Post.all
   end
@@ -80,7 +28,7 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-    @post.update params.require(:post).permit(:name, :content, :images)
+    @post.update params.require(:post).permit(:name, :content, :image)
     if @post.update(post_params)
       redirect_to post_path(@post), notice: '更新できました'
     else
