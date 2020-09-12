feb1656c1a6249de5c214b55eca51628dcc5ab4b
daisuke sasaki <gerund25495641@gmail.com>
2020-09-12 21:47:11 +0900

    Webpackをインストール

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,9 +9,10 @@ require("@rails/activestorage").start()
 require("channels")
 
 //= require jquery
-$(function(){
-    setTimeout("$('.flash.time-limit').fadeOut('slow')", 100) 
-  })
+//= require jquery_ujs
+// $(function(){
+//     setTimeout("$('.flash.time-limit').fadeOut('slow')", 100) 
+//   })
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -19,3 +20,9 @@ $(function(){
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+require("trix")
+
+document.addEventListener('DOMContentLoaded', () => {
+  
+  window.alert('DOM LOADED')
+})
