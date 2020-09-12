40016018e6488e60b65f8cd74a17a37ae4d653cf
daisuke sasaki <gerund25495641@gmail.com>
2020-09-13 07:58:56 +0900

    routers,.rbに以下のコードを追加し、sign_outできることを動作確認済み
      devise_scope :account do
        get '/accounts/sign_out' => 'devise/sessions#destroy'
      end

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
   devise_for :accounts
+  devise_scope :account do
+    get '/accounts/sign_out' => 'devise/sessions#destroy'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :posts
   root to: 'posts#index'
