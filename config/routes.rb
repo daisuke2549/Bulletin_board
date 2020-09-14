35a7acd045263f843885ffdb2ba8c843e8d464ea
daisuke sasaki <gerund25495641@gmail.com>
2020-09-14 18:19:35 +0900

    記事に対して、いいね機能を実装。動作確認済み

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
     get '/accounts/sign_out' => 'devise/sessions#destroy'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :posts
+  resources :posts do 
+    resources :comments, only: [:new,:create]
+    resource :like, only: [:create]
+  end 
+
   root to: 'posts#index'
 end
